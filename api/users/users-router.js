b7dde43373f3dabb4fd95d6e015d198afaa6c7a7
Andrewschaer <andrewschaer@github.com>
2021-09-16 00:14:37 -0400

    server connected - users router and users model db access functions built out

--- api/users/users-router.js
+++ api/users/users-router.js
@@ -1,6 +1,19 @@
 // Require the `restricted` middleware from `auth-middleware.js`. You will need it here!
+const router = require('express').Router();
 
+// const { restricted } = require('../auth/auth-middleware');
 
+const Users = require('./users-model');
+
+router.get("/", (req, res, next) => { // <= ADD MIDDLEWARE
+  Users.find()
+    .then(users => {
+      res.status(200).json(users);
+    })
+    .catch(next);
+});
+
+module.exports = router;
 /**
   [GET] /api/users
 
